e92e4cc23f8c907cff5f19ec143260dbb7fb8748
Atty Cronin <antoniajcronin@gmail.com>
2023-07-12 19:04:29 +0100

    Add Dog cards to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { useState } from 'react';
-import catData from './data/catData';
 import './App.css';
 import Navbar from './components/navbar';
 import Header from './components/header';
 import Footer from './components/footer';
 import Cat from './data/cat';
+import catData from './data/catData';
 import CatCard from './components/cat_card';
+
+import Dog from './data/dog';
+import dogData from './data/dog-data';
+import DogCard from './components/dog_card';
 
 function App(): JSX.Element {
   const [cats, setCats] = useState<Array<Cat>>(catData);
+  const [dogs, setDogs] = useState<Array<Dog>>(dogData);
 
   const numberOfCats = cats.length;
 
@@ -21,6 +26,9 @@ function App(): JSX.Element {
         <div className='cards__wrapper'>
           {cats.map((cat, index) => (
             <CatCard catObject={cat} catIndex={index} key={cat.id} />
+          ))}
+          {dogs.map((dog, index) => (
+            <DogCard dogObject={dog} dogIndex={index} key={dog.id} />
           ))}
         </div>
       </main>
